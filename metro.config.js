576e85f136470c367251de0ef13021e0fe8db1c3
harryyking <arthurharry06@gmail.com>
2025-10-08 04:43:12 +0000

    add sentry config

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
-const { withSentryMetroConfig } = require('@sentry/react-native/metro');
+const { withSentryConfig } = require('@sentry/react-native/metro');
 
 const config = getDefaultConfig(__dirname);
 
-
-// module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
-module.exports = withSentryMetroConfig(config)
+module.exports = withSentryConfig(config)
